1c7b854329b543e17e0e2572f96f9537045dd0e1
Jared <brymerjared@gmail.com>
2023-08-19 13:27:14 -0500

    required fs inquirer and the shapes / added questions for logos

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+const { Triangle, Circle, Square } = require('./lib/shapes');
+
+const questions = [
+    {
+        type: 'input',
+        name: 'text',
+        message: 'What text would you like your logo to have? (Enter 3 characters only)'
+    },
+    {
+        type: 'input',
+        name: 'text-color',
+        message: 'What would you like the color of your text to be? (Enter color keyword or a hexadecimal number)'
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'Choose a shape for your logo!',
+        choices: ['Triangle', 'Circle', 'Square']
+    },
+    {
+        type: 'input',
+        name: 'shape-color',
+        message: 'What color would you like your shape to be? (Enter color keyword or a hexadecimal number)'
+    }
+];
+
